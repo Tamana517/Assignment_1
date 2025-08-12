198ac05100ca54346e7d41c1139f76f837c93be7
Tamana517 <tamana7522@gmail.com>
2025-08-12 17:39:52 +0530

    Create Q4(e).cpp

---
+++ Q4(e).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str[100];
+    cout << "Enter a string in uppercase: ";
+    cin.getline(str, 100); // Input string
+
+    int i = 0;
+    // Traverse each character
+    while (str[i] != '\0') {
+        // If character is uppercase letter
+        if (str[i] >= 'A' && str[i] <= 'Z')
+            str[i] = str[i] + 32; // Convert to lowercase
+        i++;
+    }
+
+    cout << "String in lowercase: " << str << endl; // Output result
+    return 0;
+}
