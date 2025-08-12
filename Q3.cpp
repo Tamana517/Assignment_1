85029eef4988accf0acde1d51895f2ae82341f13
Tamana517 <tamana7522@gmail.com>
2025-08-12 18:09:21 +0530

    Update Q3.cpp

--- Q3.cpp
+++ Q3.cpp
@@ -1,3 +1,15 @@
+/*
+Q3: Predict the output of the following program:
+
+int main() {
+    int i;
+    int arr[5] = {1};
+    for (i = 0; i < 5; i++)
+        printf("%d", arr[i]);
+    return 0;
+}
+*/
+
 #include <iostream>
 using namespace std;
 
