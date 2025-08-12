c767af6995529e8b6aa454dbbd2c81b7c423e861
Tamana517 <tamana7522@gmail.com>
2025-08-12 13:26:01 +0530

    Create Q5.cpp

---
+++ Q5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cout << "Enter rows and columns: ";
+    cin >> rows >> cols;
+
+    int arr[10][10];
+
+    // Input elements
+    cout << "Enter elements:\n";
+    for(int i=0; i<rows; i++)
+        for(int j=0; j<cols; j++)
+            cin >> arr[i][j];
+
+    // Sum of each row
+    for(int i=0; i<rows; i++) {
+        int rowSum = 0;
+        for(int j=0; j<cols; j++)
+            rowSum += arr[i][j]; // Add row elements
+        cout << "Sum of row " << i+1 << " = " << rowSum << "\n";
+    }
+
+    // Sum of each column
+    for(int j=0; j<cols; j++) {
+        int colSum = 0;
+        for(int i=0; i<rows; i++)
+            colSum += arr[i][j]; // Add column elements
+        cout << "Sum of column " << j+1 << " = " << colSum << "\n";
+    }
+
+    return 0;
+}
