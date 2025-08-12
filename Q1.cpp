081f056dd8be94fc429757ff9af61f81d23fcdee
Tamana517 <tamana7522@gmail.com>
2025-08-12 13:33:00 +0530

    Create Q1.cpp

---
+++ Q1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, key;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[50];
+    cout << "Enter elements in sorted order:\n";
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    int low = 0, high = n - 1;
+    bool found = false;
+
+    // Binary Search Loop
+    while(low <= high) {
+        int mid = (low + high) / 2; // Find middle index
+
+        if(arr[mid] == key) {       // If found
+            cout << "Element found at position " << mid+1 << "\n";
+            found = true;
+            break;
+        }
+        else if(arr[mid] < key)     // Search right half
+            low = mid + 1;
+        else                        // Search left half
+            high = mid - 1;
+    }
+
+    if(!found)
+        cout << "Element not found\n";
+
+    return 0;
+}
