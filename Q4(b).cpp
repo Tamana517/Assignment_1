4739236ba0b226303e1af83df9faaff84526c076
Tamana517 <tamana7522@gmail.com>
2025-08-12 10:30:21 +0530

    Create Q4(b).cpp

---
+++ Q4(b).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void multiplyMatrices() {
+    int a[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}; // Matrix A
+    int b[3][3] = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}}; // Matrix B
+    int result[3][3]; // Result matrix
+
+    cout << "Matrix A * Matrix B =" << endl;
+
+    // Multiply matrices
+    for(int i=0; i<3; i++) {
+        for(int j=0; j<3; j++) {
+            result[i][j] = 0;
+            for(int k=0; k<3; k++) {
+                result[i][j] += a[i][k] * b[k][j]; // Dot product
+            }
+            cout << result[i][j] << " "; // Print result
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    multiplyMatrices(); // Call multiplication
+    return 0;
+}
