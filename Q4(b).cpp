ec5b807ac157923ff6ea615e4f941087e0f055f4
Tamana517 <tamana7522@gmail.com>
2025-08-12 17:34:18 +0530

    Create Q4(b).cpp

---
+++ Q4(b).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str[100];
+    cout << "Enter a string: ";
+    cin.getline(str, 100); // Input string
+
+    // Find length manually
+    int len = 0;
+    while (str[len] != '\0')
+        len++;
+
+    // Reverse by swapping characters
+    for (int i = 0; i < len / 2; i++) {
+        char temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
+
+    cout << "Reversed string: " << str << endl; // Output result
+    return 0;
+}
