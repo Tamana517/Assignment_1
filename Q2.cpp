3d421be47a9fe16f605df2dcf185fa5dd5f2b466
Tamana517 <tamana7522@gmail.com>
2025-08-12 13:38:38 +0530

    Create Q2.cpp

---
+++ Q2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false; // Reset the swapped flag
+        // Last i elements are already in place
+        for (int j = 0; j < n - i - 1; j++) {
+            // Swap if the element found is greater than the next element
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true; // Set the swapped flag
+            }
+        }
+        // If no two elements were swapped, the array is sorted
+        if (!swapped) {
+            break;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: \n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
