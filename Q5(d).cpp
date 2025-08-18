203473d221f80a49a96b790437a2037ded6f21b4
Tamana517 <tamana7522@gmail.com>
2025-08-18 15:55:24 +0530

    Create Q5(d).cpp

---
+++ Q5(d).cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of matrix: ";
+    cin >> n;
+
+    int upper[n*(n+1)/2]; // store only upper part
+    int k = 0;            // index for storage
+
+    cout << "Enter elements (row major):\n"; // input in row-major form
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int val;
+            cin >> val;
+            if (i <= j) upper[k++] = val; // keep only upper triangle
+        }
+    }
+
+    cout << "\nThe matrix is:\n";
+    k = 0; // reset index
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i <= j)
+                cout << upper[k++] << " "; // print stored element
+            else
+                cout << "0 ";              // lower part is zero
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
