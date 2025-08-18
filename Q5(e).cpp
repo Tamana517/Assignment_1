3a09206ff1b926a44d58a9e5753913077894023a
Tamana517 <tamana7522@gmail.com>
2025-08-18 17:46:58 +0530

    Update Q5(e).cpp

--- Q5(e).cpp
+++ Q5(e).cpp
@@ -1,3 +1,9 @@
+/*
+Q5: Assuming array is used to store elements of the following matrices,
+implement an efficient way that reduces the space requirement.
+(e) Symmetric Matrix.
+*/
+
 #include <iostream>
 using namespace std;
 
