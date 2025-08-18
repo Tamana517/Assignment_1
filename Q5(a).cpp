452416b3e14b9a7222fe115d9bb34034516c4e2c
Tamana517 <tamana7522@gmail.com>
2025-08-18 15:44:33 +0530

    Create Q5(a).cpp

---
+++ Q5(a).cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of matrix: ";
+    cin >> n;
+
+    int diag[n]; // store diagonal elements only
+
+    cout << "Enter elements (row major):\n"; // input in row-major form
+    for (int i = 0; i < n; i++) {
+        cin >> diag[i]; // input only diagonal
+    }
+
+    cout << "\nThe matrix is:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) cout << diag[i] << " "; // diagonal
+            else cout << "0 ";                  // non-diagonal
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
